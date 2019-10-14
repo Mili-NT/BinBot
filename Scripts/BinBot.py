ffad857621865cf38d351c6b6266cc5fc174ba14
KuteKetX <kuteketbhop@gmail.com>
2019-10-14 09:04:38 -0600

    Didn't you read the license? GPL can't be put into affect without this bro.

--- Scripts/BinBot.py
+++ Scripts/BinBot.py
@@ -1,3 +1,19 @@
+# A small python script to scrape the public pastebin archive.
+# Copyright (C) 2019  Mili
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import re
 import lib
 import codecs
@@ -50,8 +66,8 @@ AccessDeniedError = "access denied"
 def archive_connect():
     def print_connecterror():
         lib.PrintError(f"""
-    Exception occurred: {e} 
-    Possible causes: Poor/Non-functioning Internet connection or pastebin is unreachable 
+    Exception occurred: {e}
+    Possible causes: Poor/Non-functioning Internet connection or pastebin is unreachable
     Possible fixes: Troubleshoot internet connection or check status of {archive_url}
             """)
     def print_timeouterror():
@@ -116,7 +132,7 @@ def parameter_connect(proch):
     def print_connecterror():
         lib.PrintError(f"""
     Exception occurred: {e}
-    Possible causes: Poor/Non-functioning Internet connection or pastebin is unreachable 
+    Possible causes: Poor/Non-functioning Internet connection or pastebin is unreachable
     Possible fixes: Troubleshoot internet connection or check status of {archive_url}
             """)
     def print_timeouterror():
